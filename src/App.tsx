6c277c7e03059f9be7d062a7e38fc9d860a7f45f
abunaserdipu <abunaserdipu0@gmail.com>
2021-07-25 18:43:50 +0600

    launch todo list app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC,useState,ChangeEvent} from 'react';
 import './App.css';
 
-function App() {
+const App : FC = () => {
+  const [task,setTask] = useState("");
+  const [deadline,setDeadline] = useState(0);
+  const [todo,setTodo] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if(event.target.name === "task"){
+      setTask(event.target.value)
+    }else {
+      setDeadline(Number(event.target.value))
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputContainer">
+        <input type="text" name="task" onClick={handleChange} placeholder="Task..." />
+        <input type="number" name="deadline" onClick={handleChange} placeholder="Deadline (in Days)..." />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
     </div>
   );
 }
